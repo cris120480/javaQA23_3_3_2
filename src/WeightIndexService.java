b66c365fde2fceb1bc32f9f3d64f5dbc1c0f1057
Денис Гришкевич <cris12@mail.ru>
2021-09-27 22:10:06 +0300

    first commit

---
+++ src/WeightIndexService.java
@@ -0,0 +1,7 @@
+public class WeightIndexService {
+    public float calculate(int weight, float height) {
+            float miles = weight / ((height * height)  / 10000); {
+            }
+            return miles;
+        }
+}
